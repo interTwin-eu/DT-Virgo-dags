b4b5d72063a5f0a3f22dcb0a698ca179decb2a90
Sara Vallero <svallero@to.infn.it>
2024-01-11 16:12:58 +0100

    Fix typo

--- hello_world_airflow_dag.py
+++ hello_world_airflow_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def helloWorld():
-    print(‘Hello World’)
+    print("Hello World")
 
 with DAG(dag_id="hello_world_dag",
          start_date=datetime(2021,1,1),
@@ -15,6 +15,3 @@ with DAG(dag_id="hello_world_dag",
                 python_callable=helloWorld)
 
 task1
-
-
-
