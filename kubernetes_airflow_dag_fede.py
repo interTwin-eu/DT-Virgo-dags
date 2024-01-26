7dcf6e868bf76f3062f1e06086669cd253a38a63
leggerf <41438017+leggerf@users.noreply.github.com>
2024-01-26 09:53:06 +0100

    Update kubernetes_airflow_dag_fede.py

--- kubernetes_airflow_dag_fede.py
+++ kubernetes_airflow_dag_fede.py
@@ -1,20 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 """
 This is an example dag for using a Local Kubernetes Executor Configuration.
 """
@@ -89,16 +72,8 @@ if k8s:
             print(ds)
             return "Done with task_with_local_executor"
 
-        # Task with docker image
-
-        @task.docker(image="ubuntu", multiple_outputs=True)
-        def task_with_docker():
-            """A simple task with docker image"""
-           
-            return "Done with task_with_docker"
-
         ###########################
         #        DEFINE TASK ORDER
         ###########################
                 
-        task_with_docker() #>> task_with_local() >> task_with_executor()
+        task_with_local() >> task_with_executor()
