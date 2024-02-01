247a25a6dff3ebb5be0fd7a9b933f0130bd22d1d
leggerf <41438017+leggerf@users.noreply.github.com>
2024-02-01 15:31:49 +0100

    Update dags_airflow_fede_rucio_k8soperator.py

--- dags_airflow_fede_rucio_k8soperator.py
+++ dags_airflow_fede_rucio_k8soperator.py
@@ -6,10 +6,9 @@ from kubernetes.client import models as k8s
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2020, 1, 1),
-    'tags': ['fede'],
 }
 
-dag = DAG('rucio_operator', default_args=default_args, schedule_interval=None)
+dag = DAG('rucio_operator', default_args=default_args, tags=["fede"], schedule_interval=None)
 
 repo = "leggerf/rucio-intertwin"
 tag = "0.0.0"
@@ -19,8 +18,9 @@ k = KubernetesPodOperator(
     image=f"{repo}:{tag}",  # image="ubuntu:16.04",
     image_pull_secrets=[k8s.V1LocalObjectReference("dockerhub")],
     # image_pull_policy="Always",
-    cmds=["bash", "-cx"],
-    arguments=["pwd", "ls"],
+    cmds=["./get-token.sh"],
+    # cmds=["bash", "-cx"],
+    # arguments=["pwd", "ls"],
     labels={"foo": "bar"},
     name="test-data-access",
     task_id="data-access",
