ea179ea225e6b91057fa8713976529c53630b4d0
leggerf <41438017+leggerf@users.noreply.github.com>
2024-02-01 16:28:39 +0100

    Update dags_airflow_fede_rucio_k8soperator.py

--- dags_airflow_fede_rucio_k8soperator.py
+++ dags_airflow_fede_rucio_k8soperator.py
@@ -2,23 +2,22 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-
 from kubernetes.client import models as k8s
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2020, 1, 1),
+    "owner": "airflow",
+    "start_date": datetime(2020, 1, 1),
 }
 
 dag = DAG(
-    'rucio_operator', default_args=default_args, tags=["fede"], schedule_interval=None
+    "rucio_operator", default_args=default_args, tags=["fede"], schedule_interval=None
 )
 
 repo = "leggerf/rucio-intertwin"
 tag = "0.0.0"
 
 k = KubernetesPodOperator(
-    namespace='airflow',
+    namespace="airflow",
     image=f"{repo}:{tag}",  # image="ubuntu:16.04",
     image_pull_secrets=[k8s.V1LocalObjectReference("dockerhub")],
     # image_pull_policy="Always",
@@ -31,7 +30,7 @@ k = KubernetesPodOperator(
     is_delete_operator_pod=False,
     hostnetwork=False,
     startup_timeout_seconds=900,
-    dag=dag
+    dag=dag,
 )
 
 # define DAG pipeline
