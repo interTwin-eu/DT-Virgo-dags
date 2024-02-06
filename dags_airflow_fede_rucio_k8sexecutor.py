ddf552baa16edeeaa4d95ef5135b3d9ff701285a
leggerf <41438017+leggerf@users.noreply.github.com>
2024-02-06 18:09:37 +0100

    Update dags_airflow_fede_rucio_k8sexecutor.py

--- dags_airflow_fede_rucio_k8sexecutor.py
+++ dags_airflow_fede_rucio_k8sexecutor.py
@@ -62,7 +62,7 @@ if k8s:
                         k8s.V1Container(
                             name="sidecar",
                             image=f"{repo}:{tag}",
-                            args=['pwd'],
+                            args=['ls'],
                             command=["bash", "-cx"],
                             # command=["./get-token.sh"],
                             image_pull_policy="Always",
