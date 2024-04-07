7d34c447de1dd0474f5fe4c59fcd3273f736d7e7
Alessio Romano <alessio.romano329@edu.unito.it>
2024-04-07 13:48:37 +0200

    Added comments

--- dags_airflow_ale_fsensor_exmp.py
+++ dags_airflow_ale_fsensor_exmp.py
@@ -1,6 +1,9 @@
 """
-This is an example using a FileSensor
+This is an example using a FileSensor. 
+In order to work this DAG needs a connection defined as working directory.
+See the airflow UI.
 
+FileSensor expects a file in the local filesystem.
 
 
 """
@@ -16,6 +19,7 @@ dag = DAG(
     schedule_interval=None,
     description="A demonstration DAG using FileSensor.",
     
+    #DAG execution depends on previous run
     #default_args={"depends_on_past": True},
 )
 
@@ -26,10 +30,15 @@ create_metrics = DummyOperator(task_id="create_metrics", dag=dag)
 for f_id in [1, 2, 3]:
     wait = FileSensor(
         task_id=f"wait_for_file_{f_id}",
+        #define a connection
         fs_conn_id="temp_data",
         filepath=f"d_{f_id}/data.csv",
+        #condition will be checked every ten seconds
         poke_interval=10,
+        #maximum waiting time
         timeout=180,
+        #we can reschedule the task every time the condition is not met
+        #mode="reschedule"
 
         dag=dag,
     )
